8d743f0bcf7c8f904a61d1f858bca26d82107d83
Henri <henritom1301@gmail.com>
2022-10-09 15:49:01 +0700

    TGS6_C_0781

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Models\Pegawai;
 use Illuminate\Http\Request;
 
@@ -19,6 +20,74 @@ class PegawaiController extends Controller
         return view('pegawai.index', compact('pegawai'));
      }
 
+      /**
+      * create
+      *
+      * @return void
+      */
+      public function create()
+      {
+        
+         return view('pegawai.create');
+      }
+
+   /**
+      * store
+      *
+      * @param Request $request
+      * @return void
+   */
+  public function store(Request $request){
+         //Validasi Formulir
+         $this->validate($request, [
+         'nomor_induk_pegawai' => 'required',
+         'nama_pegawai' => 'required|max:15',
+         'email' => 'required|email',
+         'telepon' => 'required|between:10,13',
+         
+      ]);
+
+      //Fungsi Simpan Data ke dalam Database
+      Pegawai::create([
+         'nomor_induk_pegawai' => $request->nomor_induk_pegawai,
+         'nama_pegawai'    => $request->nama_manager,
+         'email'  => $request->email,
+         'telepon'  => $request->telepon,
+      ]);
+
+      return redirect()->route('pegawai.index')->with(['success' => 'Data Berhasil Disimpan']);
+  }
+     public function edit($id)
+     {
+       $pegawai = Pegawai::where('id', $id)->first();
+       return view('pegawai.update')->with('pegawai', $pegawai);
+     }
+ 
+      /**
+       * store
+       *
+       * @param Request $request
+       * @return void
+    */
+       public function update(Request $request, $id){
+          //Validasi Formulir
+          $this->validate($request, [
+          'nomor_induk_pegawai' => 'required',
+          'nama_pegawai'=>'required|max:15',
+          'email' => 'required|email',
+          'telepon' => 'required|between:10,13',
+       ]);
+
+       Pegawai::find($id)->update([
+         'nomor_induk_pegawai' => $request->nomor_induk_pegawai,
+         'nama_pegawai'    => $request->nama_manager,
+         'email'  => $request->email,
+         'telepon'  => $request->telepon,
+       ]);
+ 
+       return redirect()->route('pegawai.index')->with(['success' => 'Data berhasil diedit']);
+    }
+    
      public function destroy($id)
      {
         $pegawai = Pegawai::where('id',$id)->firstorfail()->delete();
